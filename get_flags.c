c078707ce9d7e7244e02f26b24af68fc2d439996
Kennedy Lelei <kennedylelei26@gmail.com>
2023-04-25 13:53:04 -0700

    Removed trailing whitespace in get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -4,8 +4,7 @@
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
- * 
- * Return: Flags:
+ * Return: Flags.
  */
 int get_flags(const char *format, int *i)
 {
@@ -24,6 +23,7 @@ int get_flags(const char *format, int *i)
 				flags |= FLAGS_ARR[j];
 				break;
 			}
+
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
